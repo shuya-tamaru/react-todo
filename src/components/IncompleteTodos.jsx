f19906929c90cf3353026488938e627d5dbc93e9
shuya-tamaru <57956439+shuya-tamaru@users.noreply.github.com>
2022-05-16 18:51:47 +0900

    Initial commit

---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const IncompleteTodos = (props) => {
+  const { todos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完のTODO</p>
+      <ul>
+        {todos.map((todos, index) => {
+          return (
+            <div key={todos} className="list-row">
+              <li>{todos}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
